d30c2630d3b1951cfe09b42b03298c1e3143e645
Takuya Shinjo <e175769@ie.u-ryukyu.ac.jp>
2020-11-12 15:34:03 +0900

    コードの修正

--- ABC182/ABC_D.py
+++ ABC182/ABC_D.py
@@ -1,3 +1,4 @@
+import itertools
 N= int(input())
 A = list(map(int, input().split()))
 cumsum = list(itertools.accumulate(A))
@@ -7,6 +8,6 @@ ans = 0
 _sum = 0
 for n in range(N):
     maxP = max(cumsum[n], maxP)
-    ans = max(ans, sum+maxP)
+    ans = max(ans, _sum+maxP)
     _sum += cumsum[n]
 print(ans)
