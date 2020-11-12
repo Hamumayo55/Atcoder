c5c95c05b67b8f9963c74909f7c5a31a2f546e18
Takuya Shinjo <e175769@ie.u-ryukyu.ac.jp>
2020-11-12 12:53:53 +0900

    add code

---
+++ ABC182/ABC_D.py
@@ -0,0 +1,12 @@
+N= int(input())
+A = list(map(int, input().split()))
+cumsum = list(itertools.accumulate(A))
+p = 0
+maxP = 0
+ans = 0
+_sum = 0
+for n in range(N):
+    maxP = max(cumsum[n], maxP)
+    ans = max(ans, sum+maxP)
+    _sum += cumsum[n]
+print(ans)
