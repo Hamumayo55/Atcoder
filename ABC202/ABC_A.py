ecbb56b102967fc073d61892e203a60fc4ce031a
Takuya Shinjo <e175769@ie.u-ryukyu.ac.jp>
2021-05-23 16:13:59 +0900

    add code

---
+++ ABC202/ABC_A.py
@@ -0,0 +1,6 @@
+#N = int(input())
+a,b,c=(int(x) for x in input().split())
+
+# A = [int(x) for x in input().split()]
+
+print(abs(7-a)+abs(7-b)+abs(7-c))
---
+++ ABC202/ABC_B.py
@@ -0,0 +1,3 @@
+S = input()
+r_S = S[::-1]
+print(r_S.replace('6','X').replace('9','6').replace('X','9'))
